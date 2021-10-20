7af612cfefd58b76574436f9fcd21c67788254e2
tanakorn0314 <tanakorn0412@gmail.com>
2021-10-20 15:39:59 +0700

    Adjusted web interaction

--- frontend/src/Ballot.tsx
+++ frontend/src/Ballot.tsx
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import signer from "../utils/provider";
+import { getSigner } from "../utils/provider";
 import { address } from './contracts/Ballot-address.json';
 import { abi } from './contracts/Ballot-abi.json';
 import { Ballot3 } from "../../src/types/Ballot3";
@@ -10,6 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import Heading from './Heading';
 import ProposalsFC from "./Proposals";
+import { JsonRpcSigner } from "@ethersproject/providers";
 
 let ballotContract: Ballot3;
 
@@ -22,21 +23,45 @@ const BallotFC: React.FC = () => {
     const [accountRight, setAccountRight] = useState('');
     const [accountDelegate, setAccountDelegate] = useState('');
 
-    const [voter, setVoter] = useState({ weight: 0, voted: "", vote: "" });
-    const [winner, setWinner] = useState('');
-
     const [user, setUser] = useState('');
-    const [proposals, setProposals] = useState<ProposalType[]>([]);
 
-    // useEffect(() => {
-    //     connect();
-    // }, [user]);
+    const [proposals, setProposals] = useState<ProposalType[]>([]);
+    const [voter, setVoter] = useState({ weight: 0, voted: false, vote: 0, delegate: "" });
+    const [chairPerson, setChairPerson] = useState('');
+
+    const [signer, setSigner] = useState<JsonRpcSigner>();
+
+    useEffect(() => {
+        if (window.ethereum) {
+            getSigner().then(initialSigner => {
+                initialSigner && setSigner(initialSigner);
+            })
+        }
+    }, []);
+
+    useEffect(() => {
+        connect();
+        window.ethereum.on('connect', connect);
+        window.ethereum.on('accountsChanged', connect);
+
+        return () => {
+            window.ethereum.removeListener('connect', connect);
+            window.ethereum.removeListener('accountsChanged', connect);
+        }
+    }, [signer]);
 
     async function connect() {
-        // console.log('signer:  ', await signer.getAddress())
-        setUser(await signer.getAddress());
-        ballotContract = new ethers.Contract(address, abi, signer) as Ballot3;
+        if (signer) {
+            setUser(await signer.getAddress());
+            ballotContract = new ethers.Contract(address, abi, signer) as Ballot3;
+            loadData();
+            loadChairPerson();
+        }
+    }
+
+    const loadData = () => {
         initProposals();
+        initVoterState();
     }
 
     async function initProposals() {
@@ -46,62 +71,23 @@ const BallotFC: React.FC = () => {
         setProposals(temp_prop);
     }
 
-    function printProposal() {
-        return proposals.map((proposal, proposalIndex) => {
-            const name = ethers.utils.parseBytes32String(proposal.name).toString();
-            const url = `/photos/${name}.png`;
-            return <ProposalsFC
-                proposal={proposal} key={proposalIndex}
-                proposalIndex={proposalIndex}
-                name={name}
-                url={url}
-                vote={vote} />
-        })
-    }
-
-    function printVoters() {
-        return (
-            <div className="px-4 py-2 rounded overflow-hidden mr-8 shadow-lg">
-                <div className="font-bold mt-4">
-                    <button
-                        className="bg-indigo-300 hover:bg-indigo-500 divide-dashed p-2 rounded mb-2"
-                        onClick={() => setVoterState()}>Get Voter Profile</button>
-                </div>
-                <div>Weight: {voter.weight}</div>
-                <div>Voted: {voter.voted}</div>
-                <div>Vote for: {voter.vote}</div>
-            </div>
-        )
-    }
-
-    function printGetWinner() {
-        return (
-            <div className="px-4 py-2 rounded overflow-hidden mr-8 shadow-lg">
-                <button
-                    className="mt-4 bg-red-600 hover:bg-red-700 p-2 text-white rounded"
-                    onClick={() => winnerName()}>Get Winner</button>
-                <div className="text-lg mt-4 ml-1"> {winner}</div>
-            </div>
-        )
-    }
-
-    async function setVoterState() {
-        if (!ballotContract) {
-            toast.warn("Please connect wallet!");
-            return;
+    async function initVoterState() {
+        if (signer) {
+            const accountAddress = await signer.getAddress();
+            console.log('address acc: ', accountAddress);
+            const voterData = await ballotContract.voters(accountAddress.toString());
+            setVoter({
+                weight: +voterData.weight,
+                voted: voterData.voted,
+                delegate: voterData.delegate,
+                vote: +voterData.vote
+            })
         }
+    }
 
-        const accountAddress = await signer.getAddress();
-        console.log('address acc: ', accountAddress);
-        const vote = await ballotContract.voters(accountAddress.toString());
-        const proposalId = proposals[+vote.vote.toString()].name;
-        const name = (vote.voted) ?
-            ethers.utils.parseBytes32String(proposalId) : "";
-        setVoter({
-            weight: +vote.weight.toString(),
-            voted: (vote.voted ? "Voted" : "Not Voted yet"),
-            vote: name
-        })
+    async function loadChairPerson() {
+        const res = await ballotContract.chairperson();
+        setChairPerson(res);
     }
 
     async function giveRightToVote(address: string) {
@@ -113,16 +99,16 @@ const BallotFC: React.FC = () => {
         if (!ballotContract)
             await connect();
 
-        // console.log('signer: ', signer)
-        // console.log('signer:  ', await signer.getAddress())
-        try {
-            const tx = await ballotContract.connect(signer).giveRightToVote(address);
-            await tx.wait();
-            console.log('Give right to: ', address);
-            ballotContract.on("GiveRightToVote", (to) => toast.success('Give right sucessfully to' + to));
-        } catch (e: any) {
-            // console.log('body: ', JSON.parse(e.body).error.message);
-            toast.error(e.data.message);
+        if (signer) {
+            try {
+                const tx = await ballotContract.connect(signer).giveRightToVote(address);
+                await tx.wait();
+                loadData();
+                console.log('Give right to: ', address);
+                ballotContract.once("GiveRightToVote", (to) => toast.success('Give right sucessfully to' + to));
+            } catch (e: any) {
+                toast.error(e.data.message);
+            }
         }
     }
 
@@ -131,64 +117,114 @@ const BallotFC: React.FC = () => {
             toast.warn('Please input correct delegate address');
             return;
         }
-        if (!ballotContract)
-            await connect();
-        try {
-            const tx = await ballotContract.connect(signer).delegate(address);
-            await tx.wait();
-            console.log('Delegate to: ', address);
-            ballotContract.on("Delegate", (to) => toast.success('Delegate to:' + to));
-        } catch (e: any) {
-            // console.log('body: ', JSON.parse(e.body).error.message);
-            toast.error(e.data.message);
+        if (signer) {
+            try {
+                const tx = await ballotContract.connect(signer).delegate(address);
+                await tx.wait();
+                console.log('Delegate to: ', address);
+                loadData();
+                ballotContract.once("Delegate", (to) => toast.success('Delegate to:' + to));
+            } catch (e: any) {
+                // console.log('body: ', JSON.parse(e.body).error.message);
+                toast.error(e.data.message);
+            }
         }
+
     }
 
     async function vote(proposal: number) {
-        if (!ballotContract)
-            await connect();
-        try {
-            console.log('proposal: ', proposal);
-            const tx = await ballotContract.vote(proposal);
-            await tx.wait();
-            setVoterState() // refresh voter score
-            initProposals() // refresh proposal score
-            ballotContract.on("Voted", (sender, proposal, count) => {
-                const proposalName = ethers.utils.parseBytes32String(proposals[proposal].name);
-                const res = `${sender} voted for ${proposalName} | total score: ${count}`;
-                toast.success('Vote to:' + res)
-            });
-        } catch (e: any) {
-            // console.log('e.error: ', e.data.message);
-            toast.error(e.data.message)
+        if (signer) {
+            try {
+                console.log('proposal: ', proposal);
+                const tx = await ballotContract.vote(proposal);
+                await tx.wait();
+                loadData();
+
+                ballotContract.once("Voted", (sender, proposal, count) => {
+                    const proposalName = ethers.utils.parseBytes32String(proposals[proposal].name);
+                    const res = `${sender} voted for ${proposalName} | total score: ${count}`;
+                    toast.success('Vote to:' + res)
+                });
+            } catch (e: any) {
+                toast.error(e.data.message)
+            }
         }
     }
 
-    async function winnerName() {
-        if (!ballotContract)
-            await connect();
-        const name: string = await ballotContract.winnerName();
-        console.log(ethers.utils.parseBytes32String(name));
-        setWinner(ethers.utils.parseBytes32String(name));
+    function winnerIndex() {
+        const scores = proposals.map(prop => +prop.voteCount);
+        const maxScore = Math.max(...scores);
+        const maxScoreProposals = proposals.filter(prop => +prop.voteCount === maxScore);
+        const winnerId = proposals.findIndex(prop => +prop.voteCount === maxScore);
+        if (maxScoreProposals.length === 1) {
+            return winnerId;
+        } else {
+            return -1;
+        }
+    }
+
+    function delegated() {
+        if (!voter.delegate || Number(voter.delegate) === 0) return null;
+        return voter.delegate;
+    }
+
+    function voterStatus() {
+        const delegatedTo = delegated();
+        if (delegatedTo) return `You have delegated to ${delegatedTo.slice(0, 5)}...${delegatedTo.slice(delegatedTo.length - 5)}.`;
+        else if (voter.voted) {
+            const proposal = proposals[voter.vote];
+            if (!proposal) return '';
+            const proposalName = ethers.utils.parseBytes32String(proposal.name);
+            return `You have voted to ${proposalName}.`;
+        }
+        else if (voter.weight === 0) return `You have no right to vote.`;
+        return `You have ${voter.weight} rights.`
+    }
+
+    function printProposal() {
+        return proposals.map((proposal, proposalIndex) => {
+            const name = ethers.utils.parseBytes32String(proposal.name).toString();
+            const url = `/photos/${name}.png`;
+            const isVoted = !delegated() && voter.voted;
+            const isNotSelected = isVoted && voter.vote !== proposalIndex
+            const disabled = !!delegated() || isNotSelected || voter.weight === 0;
+
+            return (
+                <ProposalsFC
+                    key={proposalIndex}
+                    proposal={proposal}
+                    proposalIndex={proposalIndex}
+                    isSelected={isVoted && voter.vote === proposalIndex}
+                    disabled={disabled}
+                    isWinner={proposalIndex === winnerIndex()}
+                    name={name}
+                    url={url}
+                    vote={vote}
+                />
+            )
+        })
     }
 
     function showAccountsFromGiveRight() {
+        const disabled = user !== chairPerson;
         return (
             <div className="flex flex-row justify-center mb-4">
                 <div className="mt-2 mr-4">
                     Give Right to:
                 </div>
                 <input
-                    className="w-1/2 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
+                    className="w-1/2 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2 "
                     type="text" onChange={(e) => setAccountRight(e.target.value)} />
                 <button
-                    className="bg-yellow-500 hover:bg-yellow-600 p-2 text-white rounded"
+                    disabled={disabled}
+                    className="bg-yellow-500 hover:bg-yellow-600 p-2 text-white rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
                     onClick={() => giveRightToVote(accountRight)}>Give Right</button>
             </div>
         );
     }
 
     function showDelegate() {
+        const disabled = voter.voted || voter.weight === 0;
         return (
             <div className="flex flex-row justify-center">
                 <div className="mt-2 mr-4">
@@ -198,7 +234,8 @@ const BallotFC: React.FC = () => {
                     className="w-1/2 shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline mr-2"
                     type="text" onChange={(e) => setAccountDelegate(e.target.value)} />
                 <button
-                    className="bg-blue-500 hover:bg-blue-600 p-2 text-white rounded"
+                    disabled={disabled}
+                    className="bg-blue-500 hover:bg-blue-600 p-2 text-white rounded disabled:bg-gray-300 disabled:cursor-not-allowed"
                     onClick={() => delegate(accountDelegate)}>Delegate</button>
             </div>
         );
@@ -216,13 +253,13 @@ const BallotFC: React.FC = () => {
                 <div className="flex">
                     {printProposal()}
                 </div>
-                <div className="mb-8 flex flex-row justify-center mt-4">
-                    {printVoters()}
-                    {printGetWinner()}
+
+                <div className="mt-16 flex flex-row justify-center font-semibold text-3xl">
+                    {voterStatus()}
                 </div>
 
             </div>
-            <div className="w-2/3">
+            <div className="w-2/3 mt-16">
                 {showAccountsFromGiveRight()}
                 {showDelegate()}
             </div>
--- frontend/src/Proposals.tsx
+++ frontend/src/Proposals.tsx
@@ -12,13 +12,22 @@ type ProposalsProps = {
     name: string;
     url: string;
     vote: (proposalId: number) => void;
+    isWinner: boolean;
+    isSelected: boolean;
+    disabled?: boolean;
 }
 
-const ProposalsFC: React.FC<ProposalsProps> = ({ proposal, proposalIndex, name, url, vote }) => {
+const ProposalsFC: React.FC<ProposalsProps> = ({ proposal, proposalIndex, name, url, isWinner, isSelected, disabled, vote }) => {
     return (
-        <div key={proposalIndex} className="max-w-xs rounded overflow-hidden mr-8 shadow-lg">
+        <div
+            key={proposalIndex}
+            className={`max-w-xs rounded overflow-hidden mr-8 shadow-lg ${isWinner && `bg-yellow-200`}`}
+        >
             <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2">{name}</div>
+                <div className='flex mr-2'>
+                    <div className="font-bold text-xl mb-2">{name}</div>
+                    {isWinner && (<img src="/photos/winner.png" alt="winner" className="w-8  h-8" />)}
+                </div>
                 <img className="rounded w-41 h-40 m-auto border border-black" src={url} alt="photo" />
             </div>
             <div className="px-6 pb-2 bg-grey-500 flex justify-around">
@@ -27,9 +36,14 @@ const ProposalsFC: React.FC<ProposalsProps> = ({ proposal, proposalIndex, name, 
                 </div>
                 <div>
                     <button
-                        className="bg-green-400 p-2 text-white
-                                            hover:bg-green-600 text-sm rounded"
-                        onClick={() => vote(proposalIndex)}> Vote
+                        disabled={disabled}
+                        className={`p-2 text-white
+                                   text-sm rounded
+                                  disabled:bg-gray-300 disabled:cursor-not-allowed
+                                  ${isSelected ? `bg-green-500 cursor-default` : `bg-green-400 hover:bg-green-600 cursor-pointer`}
+                                  `}
+                        onClick={() => !isSelected && vote(proposalIndex)}>
+                            {isSelected ? 'Voted' : 'Vote'}
                     </button>
                 </div>
             </div>
